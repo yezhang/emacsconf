4dde5b19cc8211c7ade7d8313a6d2792e21e4ae1
yezhang <yezhang989@163.com>
2025-03-30 13:41:07 +0800

    修改了笔记目录，形成独立的笔记目录，不与现有的 obsidian 目录混合； 在 org-roam 中逐渐新增笔记，然后迁移 obsidian 中的笔记过来，这样渐进式地进行笔记的融合。

--- config.el
+++ config.el
@@ -41,11 +41,11 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(make-directory "~/org/notes" t)
-(setq org-directory "~/org/notes")
+(make-directory "~/org/org-notes" t)
+(setq org-directory "~/org/org-notes")
 
-(make-directory "~/org/roam-notes" t)
-(setq org-roam-directory "~/org/roam-notes")
+(make-directory "~/org/org-roam-notes" t)
+(setq org-roam-directory "~/org/org-roam-notes")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
